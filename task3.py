03915fe8da278dd7969a8d58037d3d8d3d5d35a7
user1 <satymovaanna@gmail.com>
2018-03-17 12:36:51 +0300

    +

--- task3.py
+++ task3.py
@@ -1,1 +1,11 @@
-
+import numpy
+import scipy.linalg
+A = numpy.zeros((101,101))
+i,j = numpy.indices(A.shape)
+A[i==j+1]=1
+A[i==j]=1
+A[i==j+2]=1
+B = numpy.zeros(101)
+for i in range(101):
+	B[i] = i
+print(scipy.linalg.solve(A,B))
